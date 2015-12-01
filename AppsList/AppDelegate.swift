7f8225efd968c811ec75e0f6203eca1dc0dcd26f
sowicm <tony@guozi.cn>
2015-12-01 10:18:41 +0800

    Initial Commit

---
+++ AppsList/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AppsList
+//
+//  Created by Zhang Riyueming on 15/12/1.
+//  Copyright © 2015年 Sowicm Right. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AppsList/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AppsList
+//
+//  Created by Zhang Riyueming on 15/12/1.
+//  Copyright © 2015年 Sowicm Right. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
